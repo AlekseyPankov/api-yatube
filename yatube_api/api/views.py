e131512164d17dadfdf9f6ceadd644df3c75c6ba
Aleksey Pankov <pankov.al.al@yandex.ru>
2025-09-30 20:08:03 +0300

    delete unused libraries

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 
 from posts.models import Comment, Group, Post, User
